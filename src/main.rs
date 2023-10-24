c5ec8b0df1df976808fa95e3fa4c5e2a558c1ee3
Linus Oleander <linus.oleander@clark.de>
2023-10-24 10:13:35 +0200

    Process diff, sort, and print file extensions and paths

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,39 @@
 extern crate git2;
 
-use std::collections::BTreeMap;
-use std::env;
+use anyhow::Result;
+use anyhow::bail;
 use git2::Repository;
-use std::path::Path;
-use anyhow::{Context, Result};
+use std::collections::BTreeMap;
 
 fn main() -> Result<()> {
   let flags = git2::RepositoryOpenFlags::empty();
   let ceiling_dirs: &[&str] = &[];
-  let current_dir = std::env::current_dir().unwrap();
-  let repo = Repository::open_ext(current_dir, flags, ceiling_dirs).context("Failed to open repo")?;
+  let current_dir = std::env::current_dir()?;
+  let repo = Repository::open_ext(current_dir, flags, ceiling_dirs)?;
 
-  // Step 2: Diff
-  let head = repo.revparse_single("main").unwrap();
-  let tree = head.peel_to_tree().unwrap();
-  let diff = repo.diff_tree_to_workdir_with_index(Some(&tree), None).unwrap();
+  let head = repo.head()?;
+  let tree = head.peel_to_tree()?;
+  let diff = repo.diff_tree_to_workdir_with_index(Some(&tree), None)?;
 
-  // Step 3: Process diff and sort
   let mut map = BTreeMap::new();
 
-  diff
-    .foreach(
-      &mut |delta, _| {
-        if let Some(path) = delta.new_file().path() {
-          let extension = path.extension().unwrap_or_default().to_str().unwrap_or_default();
-          map.entry(extension.to_owned()).or_insert_with(Vec::new).push(path.to_owned());
-        }
-        true
-      },
-      None,
-      None,
-      None,
-    )
-    .unwrap();
+  diff.foreach(
+    &mut |delta, _| {
+      if let Some(path) = delta.new_file().path() {
+        let extension = path.extension().unwrap_or_default().to_str().unwrap_or_default();
+        map.entry(extension.to_owned()).or_insert_with(Vec::new).push(path.to_owned());
+      }
+      true
+    },
+    None,
+    None,
+    None,
+  )?;
 
-  // Step 4: Print
+  if map.is_empty() {
+    bail!("No files found");
+  }
+
   let mut last_ext = "";
 
   for (ext, paths) in &map {
